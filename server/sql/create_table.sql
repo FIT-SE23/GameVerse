42540a23edcf715441b03b566100e2143ab7f8ce
RachalHys <tintinmimi09@gmail.com>
2025-08-04 22:14:58 +0700

    add upvote to database

--- server/sql/create_table.sql
+++ server/sql/create_table.sql
@@ -79,8 +79,8 @@ create table if not exists "Forum" (
 
 create table if not exists "Post" (
   PostID uuid default gen_random_uuid() primary key,
-  UserID uuid,
-  ForumID uuid,
+  UserID uuid not null,
+  ForumID uuid not null,
   Content text,
   Upvote int4,
   PostDate timestamp default now(),
@@ -92,7 +92,7 @@ create table if not exists "Post" (
 create table if not exists "Comment" (
   CommentID uuid default gen_random_uuid(),
   UserID uuid,
-  ForumID uuid,
+  PostID uuid,
   Content text,
   Upvote int4,
   CommentDate timestamp default now(),
@@ -124,4 +124,31 @@ create table if not exists "User_Game" (
   primary key (UserID, GameID),
   foreign key (GameID) references "Game" (GameID) on delete cascade,
   foreign key (UserID) references "User" (UserID) on delete cascade
+);
+
+create table if not exists "Game_Upvote" (
+  UserID uuid,
+  GameID uuid,
+  
+  primary key (UserID, GameID),
+  foreign key (UserID) references "User"(UserID) on delete cascade,
+  foreign key (GameID) references "Game"(GameID) on delete cascade
+);
+
+create table if not exists "Post_Upvote" (
+  UserID uuid,
+  PostID uuid,
+
+  primary key (UserID, PostID),
+  foreign key (UserID) references "User"(UserID) on delete cascade,
+  foreign key (PostID) references "Post"(PostID) on delete cascade
+);
+
+create table if not exists "Comment_Upvote" (
+  UserID uuid,
+  CommentID uuid,
+
+  primary key (UserID, CommentID),
+  foreign key (UserID) references "User"(UserID) on delete cascade,
+  foreign key (CommentID) references "Comment"(CommentID) on delete cascade
 );
